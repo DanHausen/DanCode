08e760d37b5faefd80c6c18bd1d0ee109742e8f7
Daniel <danihausen@live.com>
2019-03-06 19:46:39 -0300

    Update

--- Bottle.cs
+++ Bottle.cs
@@ -26,7 +26,7 @@ public class Bottle : MonoBehaviour
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        _rb = GetComponent<Rigidbody>();
     }
 
     private void Update()
@@ -36,10 +36,10 @@ public class Bottle : MonoBehaviour
 
     void BoolCHanger()
     {
-        if (!torqueBool)
-            torqueBool = true;
-        else if (torqueBool)
-            torqueBool = false;
+        if (!_torqueBool)
+            _torqueBool = true;
+        else if (_torqueBool)
+            _torqueBool = false;
     }
 
     void TouchInputAndGetInput()
@@ -47,30 +47,30 @@ public class Bottle : MonoBehaviour
         if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began || Input.GetMouseButtonDown(0))
         {
 #if UNITY_EDITOR || UNITY_STANDALONE
-            startSwiping = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+            _startSwiping = Camera.main.ScreenToViewportPoint(Input.mousePosition);
 #else
             startSwiping = Camera.main.ScreenToViewportPoint(Input.GetTouch(0).position);
 
 #endif
-            touchTimeStarted = Time.time;
+            _touchTimeStarted = Time.time;
         }
         if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended || Input.GetMouseButtonUp(0))
         {
 #if UNITY_EDITOR || UNITY_STANDALONE
-            endSwiping = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+            _endSwiping = Camera.main.ScreenToViewportPoint(Input.mousePosition);
 #else
             endSwiping = Camera.main.ScreenToViewportPoint(Input.GetTouch(0).position);
 
 #endif
-            BoolCHanger();              
+            BoolCHanger();
 
-            torqueRotate = (torqueBool) ? -10 : 10; //Definido aqui para que em cada swipe a garrafa gire para um lado
+            _torqueRotate = (_torqueBool) ? -10 : 10; //Definido aqui para que em cada swipe a garrafa gire para um lado
 
             touchTimeEnded = Time.time;
-            swipeEndVector = (((endSwiping - startSwiping) / (touchTimeEnded - touchTimeStarted)) * forceToAdd); //Dividided the position vector by the time vector to get a more precise point, and then already applied the designer controlled force
+            _swipeEndVector = (((_endSwiping - _startSwiping) / (touchTimeEnded - _touchTimeStarted)) * forceToAdd); //Dividided the position vector by the time vector to get a more precise point, and then already applied the designer controlled force
 
-            rb.AddForce(0,swipeEndVector.y,0, ForceMode.Impulse);
-            rb.AddTorque(0, 0, torqueRotate, ForceMode.Impulse); //To rotate the bottle in the air
+            _rb.AddForce(0, _swipeEndVector.y,0, ForceMode.Impulse);
+            _rb.AddTorque(0, 0, _torqueRotate, ForceMode.Impulse); //To rotate the bottle in the air
         }
     }
 
